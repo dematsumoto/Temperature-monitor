9fc5dab1f63b3d505b17b4b459a7dca4c67407e3
Douglas Matsumoto <douglas.matsumoto@gmail.com>
2019-01-18 18:16:39 -0200

    fixed right temp character from rpi

--- flaskr/raspberry/temp_reader.py
+++ flaskr/raspberry/temp_reader.py
@@ -13,8 +13,8 @@ def _get_temp():
     rpi_supported = os.environ.get('RPI_SUPPORTED', 'False')
     is_compatible = True if rpi_supported.lower() == 'true' else False
 
-    temp = 'temp=61.9ºC' if not is_compatible else os.popen("vcgencmd measure_temp").readline()
-    temp = temp.replace("ºC", "")
+    temp = "temp=61.9'C" if not is_compatible else os.popen("vcgencmd measure_temp").readline()
+    temp = temp.replace("'C", "")
     return temp.replace("temp=", "")
 
 
